6e91cd6eb16f21f6d33720d0e41cba166e697d33
zuokun300 <3270477719@qq.com>
2019-12-19 21:01:47 +0800

    newbranch

---
+++ NewBranch.java
@@ -0,0 +1,13 @@
+public class NewBranch{
+
+	public static void main(String args[]){
+		System.out.println("1");
+
+		System.out.println("NewBranch");
+
+		System.out.println("NewBranch");
+
+		System.out.println("2");
+	}
+
+}
