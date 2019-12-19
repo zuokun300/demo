2a423d5cfa0b06e23b6bf276f564b8657fcd28f5
zuokun300 <3270477719@qq.com>
2019-12-19 20:44:35 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+
+	public static void main(String args[]){
+
+		System.out.println("HelloWorld");
+
+		System.out.println("1");
+
+		System.out.println("HelloWorld");
+
+		System.out.println("2");
+	}
+
+}
